67b4f625184d64a23d98d4a0b7433ed8a44f37db
Naokazu Terada <naokazu.terada@gmail.com>
2019-11-01 15:48:45 +0900

    Add ScrollGuide

---
+++ src/components/atoms/ScrollGuide.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.scroll SCROLL
+</template>
+
+<style lang="sass" scoped>
+.scroll
+  font-size: 12px
+  letter-spacing: 0.05em
+  display: flex
+  justify-content: center
+  align-items: center
+  flex-direction: column
+  z-index: 10
+  &:after
+    content: ''
+    display: block
+    width: 2px
+    height: 47px
+    margin-top: 18px
+    background-color: white
+</style>
