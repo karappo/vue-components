69dedbc2afa4a5def6a326f0c157b0936b865336
Naokazu Terada <naokazu.terada@gmail.com>
2019-11-07 22:22:33 +0900

    Add props color

--- src/components/atoms/ScrollGuide.vue
+++ src/components/atoms/ScrollGuide.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.scroll
+.scroll(:style="styles")
   span SCROLL
   i
 </template>
@@ -24,12 +24,13 @@
   span
     font-size: 12px
     letter-spacing: 0.05em
+    color: var(--color)
   i
     display: block
     width: 2px
     height: 47px
     margin-top: 18px
-    background-color: rgba(255,255,255,.4)
+    background-color: rgba(var(--color-hex), .4)
     overflow: hidden
     position: relative
     &:before
@@ -37,9 +38,36 @@
       content: ''
       width: 100%
       height: 100%
-      background-color: rgba(255,255,255,.8)
+      background-color: rgba(var(--color-hex), .8)
       position: absolute
       top: 0
       left: 0
       animation: scroll 2s infinite normal
 </style>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    color: {
+      type: String,
+      default: '#ffffff'
+    }
+  },
+  computed: {
+    styles () {
+      return {
+        '--color': this.color,
+        '--color-hex': this.hexToRgb(this.color)
+      }
+    }
+  },
+  methods: {
+    hexToRgb: (hex) => {
+      var res = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      return res ? `${parseInt(res[1], 16)}, ${parseInt(res[2], 16)}, ${parseInt(res[3], 16)}` : null;
+    }
+  }
+})
+</script>
