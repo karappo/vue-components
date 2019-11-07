a2735e6e0c347b072d7ace0ba762576e9b2ad6b5
Naokazu Terada <naokazu.terada@gmail.com>
2019-11-07 20:00:05 +0900

    Add animation

--- src/components/atoms/ScrollGuide.vue
+++ src/components/atoms/ScrollGuide.vue
@@ -1,21 +1,45 @@
 <template lang="pug">
-.scroll SCROLL
+.scroll
+  span SCROLL
+  i
 </template>
 
 <style lang="sass" scoped>
+@keyframes scroll
+	0%
+		transform: translate3d(0, -100%, 0)
+	15%
+		transform: translate3d(0, -98%, 0)
+	85%
+		transform: translate3d(0, 98%, 0)
+	100%
+		transform: translate3d(0, 100%, 0)
+
 .scroll
-  font-size: 12px
-  letter-spacing: 0.05em
   display: flex
   justify-content: center
   align-items: center
   flex-direction: column
   z-index: 10
-  &:after
-    content: ''
+  span
+    font-size: 12px
+    letter-spacing: 0.05em
+  i
     display: block
     width: 2px
     height: 47px
     margin-top: 18px
-    background-color: white
+    background-color: rgba(255,255,255,.4)
+    overflow: hidden
+    position: relative
+    &:before
+      display: block
+      content: ''
+      width: 100%
+      height: 100%
+      background-color: rgba(255,255,255,.8)
+      position: absolute
+      top: 0
+      left: 0
+      animation: scroll 2s infinite normal
 </style>
